e3b3fee80e0edcd125e33bf2e2b792e6a5484c2b
andresramires17 <andresramires17@gmail.com>
2021-10-09 04:36:34 -0500

    Upd formulario registro

--- src/MVC/vistas/paginas/registro.php
+++ src/MVC/vistas/paginas/registro.php
@@ -2,25 +2,43 @@
 $registro = ControladorFormulario::ctrRegistro();
 
 if ($registro == "ok") {
-    
+
     echo '<div class="alert alert-success">Registro completo</div>';
 }
 
 ?>
-<h1>registro</h1>
-<form action="" method="post">
-    <div class="form-group">
-        <label for="email">Correo:</label>
-        <input type="email" class="form-control" placeholder="Ingrese el correo" id="email" name="RegCorreo">
-    </div>
-    <div class="form-group">
-        <label for="nombre">Nombre:</label>
-        <input type="text" class="form-control" placeholder="Ingrese el correo" id="nombre" name="RegNombre">
-    </div>
-    <div class="form-group">
-        <label for="pwd">contraseña:</label>
-        <input type="password" class="form-control" placeholder="Ingrese la contraseña" id="pwd" name="RegPass">
-    </div>
 
-    <button type="submit" class="btn btn-primary">Envíar</button>
-</form>
+<div class="d-flex justify-content-center text-center py-5">
+    <form class="p-5 bg-dark" style="color:white" action="" method="post" >
+
+        <div class="form-group">
+            <label for="nombre">Nombre:</label>
+            <div class="input-group">
+                <div class="input-group-prepend">
+                    <span class="input-group-text"><i class="fas fa-user-tie"></i></span>
+                </div>
+                <input type="text" class="form-control" placeholder="Ingrese el nombre" id="nombre" name="RegNombre">
+            </div>
+        </div>
+        <div class="form-group">
+            <label for="email">Correo:</label>
+            <div class="input-group">
+                <div class="input-group-prepend">
+                    <span class="input-group-text"><i class="fas fa-envelope-open-text"></i></span>
+                </div>
+                <input type="email" class="form-control" placeholder="Ingrese el correo" id="email" name="RegCorreo">
+            </div>
+        </div>
+        <div class="form-group">
+            <label for="pwd">contraseña:</label>
+            <div class="input-group">
+                <div class="input-group-prepend">
+                    <span class="input-group-text"><i class="fas fa-key"></i></span>
+                </div>
+                <input type="password" class="form-control" placeholder="Ingrese la contraseña" id="pwd" name="RegPass">
+            </div>
+        </div>
+
+        <button type="submit" class="btn btn-primary">Envíar</button>
+    </form>
+</div>
